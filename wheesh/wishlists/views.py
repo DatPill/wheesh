d2627789ec54034cc2c1486ee94d53a3b745e86d
DatPill <smartcrackerprezents@gmail.com>
2024-11-01 12:14:49 +0300

    Add context to IndexView

--- wheesh/wishlists/views.py
+++ wheesh/wishlists/views.py
@@ -1,5 +1,13 @@
+from typing import Any
+
 from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
     template_name = 'wishlists/index.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context: dict[str, Any] = super().get_context_data(**kwargs)
+
+        context['title'] = 'Wheesh'
+        return context
