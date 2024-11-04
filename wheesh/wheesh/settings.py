8b4615f86654c393e0b99d80049bc86d1cd776ba
DatPill <smartcrackerprezents@gmail.com>
2024-11-04 21:52:16 +0300

    New user model and its migration + fix wishlists.urls imports

--- wheesh/wheesh/settings.py
+++ wheesh/wheesh/settings.py
@@ -117,7 +117,15 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR /'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Default user model
+
+AUTH_USER_MODEL = 'users.User'
--- wheesh/wheesh/urls.py
+++ wheesh/wheesh/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from wishlists.views import IndexView
@@ -7,3 +9,6 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('wishlists/', include('wishlists.urls', namespace='wishlists')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
