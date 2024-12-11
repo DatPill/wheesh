96dbcf526b60bc4edcdbedf0095e44750413a6e8
DatPill <smartcrackerprezents@gmail.com>
2024-12-11 15:40:31 +0300

    Fix link lenght

--- wheesh/wishlists/forms.py
+++ wheesh/wishlists/forms.py
@@ -6,7 +6,7 @@ class NewPresentForm(forms.ModelForm):
     image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control', 'id': 'profilePicInput', 'onchange': 'previewImage(event)'}), required=False)
     title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите название подарка (макс. 40 символов)'}), max_length=40)
     description = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите краткое описание/пожелание (макс. 40 символов)'}), required=False, max_length=40)
-    link = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ссылка на подарок'}), max_length=40)
+    link = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ссылка на подарок'}))
     price = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Введите цену подарка (в рублях)'}), max_digits=9, decimal_places=2, required=False)
 
 
@@ -19,7 +19,7 @@ class EditPresentForm(forms.ModelForm):
     image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control', 'id': 'profilePicInput', 'onchange': 'previewImage(event)'}), required=False)
     title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите название подарка (макс. 40 символов)'}), max_length=40)
     description = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите краткое описание/пожелание (макс. 40 символов)'}), required=False, max_length=40)
-    link = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ссылка на подарок'}), max_length=40)
+    link = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ссылка на подарок'}))
     price = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Введите цену подарка (в рублях)'}), max_digits=9, decimal_places=2, required=False)
 
 
